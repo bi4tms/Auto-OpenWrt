54ea8279e453e8022ae7bb1a4e63a63c1e7ada62
bi4tms <110249561+bi4tms@users.noreply.github.com>
2023-07-01 01:45:48 +0800

    Update diy-5.15-part2.sh

--- diy-5.15-part2.sh
+++ diy-5.15-part2.sh
@@ -35,7 +35,7 @@ sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefil
 # sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
 ########### 安装smartdns（必选）###########
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 
 
